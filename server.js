f12bc4c4441cb936deffab84189cef89e0c041a3
Ice <iestyndevera@gmail.com>
2020-07-18 14:55:42 +0800

    fixed port and old ref db

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const { restart } = require("nodemon");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile.js");
+const image = require("./controllers/image");
+
+const PORT = process.env.PORT;
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "123",
+    database: "smartbrain_db",
+  },
+});
+
+// db.select("*")
+//   .from("users")
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  // res.send(db.users);
+  res.send("its working!");
+});
+
+app.post("/signin", signin.handleSignin(db, bcrypt));
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+});
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userid --> GET = user
+/image --> PUT user
+*/
