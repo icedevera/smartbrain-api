5347d76057a32e64066bd66370897a2b6858b45e
Ice <iestyndevera@gmail.com>
2020-07-18 19:26:40 +0800

    changed connection to database

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password ) {
-    return res.status(400).json('Incorrect Form Submission');
+    return res.status(400).json('Incorrect Form Submission', res);
   }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
